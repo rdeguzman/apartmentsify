ced07590d93df0ba1d11135048ffeec15af7411c
rupert <rupert@2rmobile.com>
2015-11-15 08:57:18 +1100

    Add validation on Property model

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,4 +1,17 @@
 class Property < ActiveRecord::Base
   belongs_to :user
   has_many :photos
+
+  validates :property_type, presence: true
+
+  validates :address, presence: true
+  validates :suburb, presence: true
+  validates :postcode, presence: true
+  validates :state, presence: true
+
+  validates :description, presence: true
+
+  validates :bedrooms, presence: true
+  validates :bathrooms, presence: true
+  validates :car_spaces, presence: true
 end
