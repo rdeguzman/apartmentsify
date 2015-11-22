c85d777ae2d50b6d0bed8a741c64042f584227c7
rupert <rupert@2rmobile.com>
2015-11-22 19:22:15 +1100

    Add registrations controller. Update phone_number and description

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
              path: '',
              path_names: {sign_in: 'login', sign_out: 'logout', edit: 'profile'},
              controllers: {
-                 omniauth_callbacks: 'omniauth_callbacks'
+                 omniauth_callbacks: 'omniauth_callbacks',
+                 registrations: 'registrations'
              }
 
 
