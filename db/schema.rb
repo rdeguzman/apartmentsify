71bc091dc27c36e407c0376a2a1ad66b45a27e7a
rupert <rupert@2rmobile.com>
2015-11-22 18:56:41 +1100

    Add extra fields to user. Include "My Profile" and "Account Settings" links in navbar

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151122012043) do
+ActiveRecord::Schema.define(version: 20151122071527) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,6 +66,8 @@ ActiveRecord::Schema.define(version: 20151122012043) do
     t.string   "provider"
     t.string   "uid"
     t.string   "image"
+    t.string   "phone_number"
+    t.text     "description"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
