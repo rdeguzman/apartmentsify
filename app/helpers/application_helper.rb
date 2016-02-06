eca7f15fe72c3e2c6e65d2f391df35194739563c
rupert <rupert@2rmobile.com>
2016-02-06 17:38:37 +1100

    Provide default-user.png if user registers normally

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,7 +5,7 @@ module ApplicationHelper
     if user.image
       user.image
     else
-      "https://www.gravatar.com/avatar/#{gravatar_id}.jpg?d=identical&s=150"
+    'default-user.png'
     end
   end
 
